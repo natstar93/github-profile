29f70a316fb84d75a9fc0578b605d81f06ecf6b3
Natalie Akam <natalie.akam@gmail.com>
2015-07-29 17:14:57 +0100

    Added protractor tests

---
+++ test/e2e/githubProfileFeature.js
@@ -0,0 +1,17 @@
+describe('Github Profile finder', function() {
+
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  });
+
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('Github user search');
+  });
+
+  it('finds profiles', function() {
+    searchBox.sendKeys('spike01');
+    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
+  });
+});
